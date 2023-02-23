5308953e09649589bae526ffe4fe00b96cddfbe4
JoffreyNK <joyjoffrey@gmail.com>
2023-02-23 11:39:00 +0200

    add left part of login interface

--- src/Login/Index.js
+++ src/Login/Index.js
@@ -2,7 +2,18 @@ import "./Index.scss"
 
 const Index = () => {
   return (
-    <div>
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Jess Talk</h1>
+                    <p>Welcome to Jess talk and share your photos, ideas, and videos with others</p>
+                </div>
+                <p>Don't you have account?</p>
+                <button>Register</button>    
+            </div>
+            <div className="right">
+                
+        </div>
       Hello World
     </div>
   )
