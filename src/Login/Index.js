d0d3b392c33056ef689fe588c59051527804434d
JoffreyNK <joyjoffrey@gmail.com>
2023-02-23 14:17:39 +0200

    add login class structure in sass

--- src/Login/Index.js
+++ src/Login/Index.js
@@ -1,27 +1,30 @@
-import "./Index.scss"
+import "./Index.scss";
 
 const Index = () => {
   return (
     <div className="login">
-        <div className="card">
-            <div className="left">
-                <h1>Jess Talk</h1>
-                    <p>Welcome to Jess talk and share your photos, ideas, and videos with others</p>
-                </div>
-                <p>Don't you have account?</p>
-                <button>Register</button>    
-            </div>
-            <div className="right">
-                <h1>Login</h1>
-                <form>
-                    <input type="text" placeholder="User name"/>
-                    <input type="password" placeholder="Password"/>
-                    <button>Login</button>
-                </form>
+      <div className="card">
+        <div className="left">
+          <h1>Jess Talk</h1>
+          <p>
+            Welcome to Jess talk and share your photos, ideas, and videos with
+            others
+          </p>
+          <span>Don't you have account?</span>
+          <button>Register</button>
         </div>
+      </div>
+      <div className="right">
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="User name" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </form>
+      </div>
       Hello World
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
