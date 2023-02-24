ca9fa8f1c99cc572a6555a037cb371d4545d4d6d
Joffreynk <joyjoffrey@gmail.com>
2023-02-25 01:27:08 +0200

    add data layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import HeaderBar from './NavBars/HeaderBar'
+import LeftBar from './NavBars/LeftBar'
+import RightBar from './NavBars/RightBar'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+        <HeaderBar />
+        <div>
+            <LeftBar />
+            <Outlet />
+            <RightBar />
+        </div>
+    </div>
   )
 }
 
