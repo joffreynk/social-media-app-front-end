530ac2c5205b8da783abb81ac77a75b9f6dba3e0
Joffreynk <joyjoffrey@gmail.com>
2023-02-25 01:35:20 +0200

    create profile file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './login&register/Register';
 import Login from './login&register/Login';
+import Layout from './Layout';
+import 
 
 function App() {
   return (
     <Router className="">
       <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/profile:id" element={<Profile/>} />
+        </Route>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register/>} />
       </Routes>
