b7f82a534f630c6cf76edac62eaedf5c62bf1e64
Joffreynk <joyjoffrey@gmail.com>
2023-02-24 23:58:13 +0200

    combine login ad register

---
+++ src/login&register/Register.js
@@ -0,0 +1,29 @@
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>J Talk</h1>
+          <p>
+            Welcome to J Talk and share your photos, ideas, and videos with
+            others
+          </p>
+          <span>Don't you have account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login <span></span> </h1>
+          <form>
+            <input type="text" placeholder="User name" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
