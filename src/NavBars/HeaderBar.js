395e493e56c2a01c3fb91979e43c386db34cd232
Joffreynk <joyjoffrey@gmail.com>
2023-02-26 21:36:45 +0200

    add header component information

--- src/NavBars/HeaderBar.js
+++ src/NavBars/HeaderBar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
@@ -7,13 +9,12 @@ import Person4OutlinedIcon from '@mui/icons-material/Person4Outlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import { Link } from 'react-router-dom';
 
 
 
 const HeaderBar = () => {
   return (
-    <div>
+    <div className='header-bar'>
       <div className='left'>
         <Link>J Talk</Link>
         <HomeOutlinedIcon />
@@ -25,7 +26,13 @@ const HeaderBar = () => {
         </div>
       </div>
       <div className='right'>
-        
+        <Person4OutlinedIcon />
+        <MailOutlineOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div>
+          {/* <img src='' alt='' /> */}
+          <span>JOffreynk</span>
+        </div>
       </div>
     </div>
   )
