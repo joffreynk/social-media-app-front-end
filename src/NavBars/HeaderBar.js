c2814b92de98f717ae5faddb94d39f1084f97025
Joffreynk <joyjoffrey@gmail.com>
2023-02-26 20:16:13 +0200

    import material ui components

--- src/NavBars/HeaderBar.js
+++ src/NavBars/HeaderBar.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import Person4OutlinedIcon from '@mui/icons-material/Person4Outlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+
+
 
 const HeaderBar = () => {
   return (
