fa73684e32cf74775aed59203bf1100fe89c6213
Joffreynk <joyjoffrey@gmail.com>
2023-02-27 23:55:45 +0200

    import left bar icons

--- src/NavBars/LeftBar.js
+++ src/NavBars/LeftBar.js
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import event from '../images/nav/event.png'
+import game from '../images/nav/game.png'
+import funds from '../images/nav/funds.png'
+import courses from '../images/nav/courses.png'
+import tutorial from '../images/nav/tutorial.png'
+import video from '../images/nav/video.png'
 
 
 const LeftBar = () => {
