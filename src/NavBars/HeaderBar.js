4764db85e7310c8906a3a584a220502a95232f19
Joffreynk <joyjoffrey@gmail.com>
2023-02-27 22:22:23 +0200

    styling the header with inner elements

--- src/NavBars/HeaderBar.js
+++ src/NavBars/HeaderBar.js
@@ -10,26 +10,28 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 
-
+import './headerBar.css'
 
 const HeaderBar = () => {
   return (
     <div className='header-bar'>
       <div className='left'>
+        <span>
         <Link>J Talk</Link>
+        </span>
         <HomeOutlinedIcon />
         <DarkModeOutlinedIcon />
         <GridViewOutlinedIcon />
-        <div>
-          <SearchOutlinedIcon />
+        <div className='search'>
           <input type="text" placeholder='Search' />
+          <SearchOutlinedIcon className='search-btn' />
         </div>
       </div>
       <div className='right'>
         <Person4OutlinedIcon />
         <MailOutlineOutlinedIcon />
         <NotificationsOutlinedIcon />
-        <div>
+        <div className='prof-icon'>
           {/* <img src='' alt='' /> */}
           <span>JOffreynk</span>
         </div>
