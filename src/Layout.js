aea6ca97eaf4d47c7390be69319f76a0143b5646
Joffreynk <joyjoffrey@gmail.com>
2023-03-01 21:51:09 +0200

    adding sticky to left bar

--- src/Layout.js
+++ src/Layout.js
@@ -8,7 +8,8 @@ const Layout = () => {
   return (
     <div>
         <HeaderBar />
-        <div style={{display:"flex", justifyContent: 'space-between', paddingTop:"0.5rem"}}>
+        <div style={{display:"flex", paddingTop:"0.5rem"}}>
+
             <LeftBar />
             <Outlet />
             <RightBar />
--- src/NavBars/RightBar.js
+++ src/NavBars/RightBar.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
+    <div className='rightBar'>
+      <h1></h1>
+    </div>
   )
 }
 
