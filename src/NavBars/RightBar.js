70c410d203464f1ea64092a9bd4f083d6b8e5dba
Joffreynk <joyjoffrey@gmail.com>
2023-03-02 00:01:48 +0200

    createright bar data

--- src/NavBars/RightBar.js
+++ src/NavBars/RightBar.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import passportImage from '../images/passport.png'
+
 const RightBar = () => {
   return (
     <div className='rightBar'>
-      <h1>Right</h1>
+      <div className='container'>
+        <div className='item'>
+          <span>Sugestions for you</span>
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+            </div>
+            <div>
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+
+
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
