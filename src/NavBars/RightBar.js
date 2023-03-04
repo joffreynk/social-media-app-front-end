56fd717db000670d6335c04d853e5ce2c8be5eb3
Joffreynk <joyjoffrey@gmail.com>
2023-03-04 22:51:44 +0200

    create multiple users on last activities section

--- src/NavBars/RightBar.js
+++ src/NavBars/RightBar.js
@@ -33,25 +33,117 @@ const RightBar = () => {
         </div>
 
         <div className='item'>
-          <span>Sugestions for you</span>
+          <span>Last activities</span>
           <div className='user'>
             <div className='user-info'>
               <img src={passportImage} alt='passport' />
               <span>Joffrey NK</span>
+              <p>liked your post</p>
             </div>
             <div className='user-buttons'>
-            <button className='follow'>Follow</button>
-              <button className='dismiss'>Dismiss</button>
+              <p>1 min ago</p>
+            </div>
+          </div>
+
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div>
+
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
             </div>
           </div>
           <div className='user'>
             <div className='user-info'>
               <img src={passportImage} alt='passport' />
               <span>Joffrey NK</span>
+              <p>liked your post</p>
             </div>
             <div className='user-buttons'>
-              <button className='follow'>Follow</button>
-              <button className='dismiss'>Dismiss</button>
+              <p>1 min ago</p>
+            </div>
+          </div>
+
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div>
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div>
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div>
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div> <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div>
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
+            </div>
+          </div>
+          <div className='user'>
+            <div className='user-info'>
+              <img src={passportImage} alt='passport' />
+              <span>Joffrey NK</span>
+              <p>liked your post</p>
+            </div>
+            <div className='user-buttons'>
+              <p>1 min ago</p>
             </div>
           </div>
         </div>
