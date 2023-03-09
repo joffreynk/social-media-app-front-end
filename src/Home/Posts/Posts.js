c9492165bd99519ea1145a1295b8606194088dc8
Joffreynk <joyjoffrey@gmail.com>
2023-03-09 23:39:58 +0200

    create posts and stroies funcion and export them

---
+++ src/Home/Posts/Posts.js
@@ -0,0 +1,9 @@
+import './posts.css'
+
+const Posts = () => {
+  return (
+    <div>Posts</div>
+  )
+}
+
+export default Posts
---
+++ src/Home/stories/Stories.js
@@ -0,0 +1,9 @@
+import './stories.css'
+
+const Stories = () => {
+  return (
+    <div>Stories</div>
+  )
+}
+
+export default Stories
