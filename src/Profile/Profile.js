0ef85a016dfde9d8b4ff3a1c060c8ea5b357f464
Joffreynk <joyjoffrey@gmail.com>
2023-03-12 10:12:17 +0200

    style profile header

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,8 +1,17 @@
-
-
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LanguageIcon from '@mui/icons-material/Language';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 import './profile.css'
 import profilePict from '../images/passport.png';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   return (
@@ -12,6 +21,27 @@ const Profile = () => {
         <img className='profilePict' src={profilePict} alt='Profile' />
       </div>
       <div className='userContainer'>
+        <div className='user'>
+          <div className='left'>
+            <Link to=''><FacebookOutlinedIcon /> </Link>
+            <Link to=''><InstagramIcon /> </Link>
+            <Link to=''><TwitterIcon /> </Link>
+            <Link to=''><LinkedInIcon /> </Link>
+            <Link to=''><PinterestIcon /> </Link>
+          </div>
+         <div className='center'>
+          <p>user Name</p>
+          <div>
+          <Link to=''><LocationOnIcon /> Bujumbura</Link>
+          <Link to=''><LanguageIcon /> Joffrrey NK</Link>
+          </div>
+          <button>Follow</button>
+         </div>
+         <div className='right'>
+          <MailOutlineIcon />
+          <MoreVertIcon />
+         </div>
+        </div>
 
       </div>
       
