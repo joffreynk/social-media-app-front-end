fa7aacfb1c7f3bbbb4ae917e66cf4b79b1b09451
Joffreynk <joyjoffrey@gmail.com>
2023-03-12 09:13:56 +0200

    add profile pictures

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,13 +2,14 @@
 
 
 import './profile.css'
+import profilePict from '../images/passport.png';
 
 const Profile = () => {
   return (
     <div className='profile'>
       <div className='images'>
-        <img className='cover' src='' alt='Profile' />
-        <img className='profilePict' src='' alt='Profile' />
+        <img className='cover' src='https://imageio.forbes.com/specials-images/dam/imageserve/1034931427/960x0.jpg?format=jpg&width=960' alt='Profile' />
+        <img className='profilePict' src={profilePict} alt='Profile' />
       </div>
       <div className='userContainer'>
 
