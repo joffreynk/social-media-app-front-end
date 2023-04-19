bdc70b078589cf7b053a898e29d8861f3d0cce17
Joffreynk <joyjoffrey@gmail.com>
2023-04-20 00:04:47 +0200

    add fetch functionality

--- src/Home/Posts/Posts.js
+++ src/Home/Posts/Posts.js
@@ -1,17 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
+
 import './posts.css'
 
 import Post from './post'
-import posts from '../data.js'
 import CreatePost from './createPost'
+import { makeRequest } from '../../context/requests'
 
 const Posts = () => {
   
+  const { isLoading, error, posts } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () =>
+    makeRequest.get('/posts').then((res) => res.json()),
+  })
+
+  if (isLoading) return 'Loading POSTS';
+
+  if (error) return 'Failed to fetch posts, please try again';
 
   return (
+
     <div className='posts'>
       <CreatePost />
      {
-      posts.map(post => <Post post={post} key={post.id} /> )
+      posts.length? posts.map(post => <Post post={post} key={post.id} /> ): "OOops you don't have a post"
      }
 
     </div>
