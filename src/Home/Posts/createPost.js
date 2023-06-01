747434d242d9091de2e4fdd0ef687659534d4ce2
Joffreynk <joyjoffrey@gmail.com>
2023-06-01 20:56:34 +0200

    fix post creation variable names

--- src/Home/Posts/createPost.js
+++ src/Home/Posts/createPost.js
@@ -7,7 +7,7 @@ const CreatePost = () => {
   const {currentUser} = useContext(AuthContext)
   const [postData, setPostData] = useState({description:null, postImage:null})
   const post = ()=>{
-        
+      console.log(postData)
     }
   return (
     <form>
@@ -15,11 +15,11 @@ const CreatePost = () => {
         <div className='img'>
         <img src={currentUser.profilePicture} alt='user profile' />
         </div>
-        <textarea rows={4}  placeholder='what is in your mind' name='description' onChange={e=>setPostData({...postData, describe:e.target.value})}></textarea>
+        <textarea rows={4}  placeholder='what is in your mind' name='description' onChange={e=>setPostData({...postData, description:e.target.value})}></textarea>
       </div>
       <div className='lower'>
-        <input type='file' accept="image/x-png,image/gif,image/jpeg"  onChange={e=>setPostData({...postData, postImage:e.target.value})} name='postImage' alt='choose picture' />
-        <button type='submit' onClick={post}>post</button>
+        <input type='file' accept="image/x-png,image/gif,image/jpeg"  onChange={e=>setPostData({...postData, postImage:e.target.files[0]})} name='postImage' alt='choose picture' />
+        <button type='button' onClick={post}>post</button>
       </div>
     </form>
   )
